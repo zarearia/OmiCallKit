682b838bc9091030830c93ce210212671b474930
aria zare <ariaaazare@gmail.com>
2022-10-09 15:55:08 +0200

    first commit

---
+++ OmiCallKit/OmiCallKit-Bridging-Header.h
@@ -0,0 +1,23 @@
+//
+//  OmiCallKit.h
+//  OmiCallKit
+//
+//  Created by QUOC VIET  on 06/10/2022.
+//
+
+#import <VialerSIPLib/VSLAudioController.h>
+#import <VialerSIPLib/VSLCall.h>
+#import <VialerSIPLib/VialerSIPLib.h>
+#import <VialerSIPLib.h>
+#import <VialerSIPLib/VSLRingtone.h>
+#import <VialerSIPLib/CallKitProviderDelegate.h>
+#import <VialerSIPLib/VSLAudioCodecs.h>
+#import <VialerSIPLib/VSLVideoCodecs.h>
+#import <VialerSIPLib/VSLOpusConfiguration.h>
+#import <VSLEndpoint.h>
+
+#ifndef OmiCallKit_h
+#define OmiCallKit_h
+
+
+#endif /* OmiCallKit_h */
